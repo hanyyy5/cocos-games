c4e032feb174df42de0a0c9625a6aa92b852aae6
yuhan <hanyustu@163.com>
2019-09-16 14:22:53 +0800

    new stick hero

--- StickHero-master/library/imports/43/4310c78e-46eb-450a-b504-0694e8cae4dd.js
+++ StickHero-master/library/imports/43/4310c78e-46eb-450a-b504-0694e8cae4dd.js
@@ -14,9 +14,47 @@ var spriteCreator = function () {
             newLand.anchorY = 0;
             var sprite = newLand.addComponent(cc.Sprite);
             sprite.sizeMode = cc.Sprite.SizeMode.CUSTOM;
-            newLand.color = cc.Color.BLACK;
+            sprite.srcBlendFactor = cc.macro.BlendFactor.ONE;
             newLand.height = 300;
             newLand.width = width;
+
+            //地面绿色块
+            var graphics = newLand.addComponent(cc.Graphics);
+            var color = cc.Color.GREEN;
+            var fillColor = color.fromHEX("#226e0b");
+            graphics.roundRect(0, 280, width, 19, 5);
+            graphics.fillColor = fillColor;
+            graphics.fill();
+
+            //地面棕色块
+            var fillColor = color.fromHEX("#4a1b18");
+            graphics.rect(5, 260, width - 10, 20);
+            graphics.fillColor = fillColor;
+            graphics.fill();
+
+            //地面白色块
+            var color = cc.Color.WHITE;
+            graphics.rect(5, 250, width - 10, 10);
+            graphics.fillColor = color;
+            graphics.fill();
+
+            //底部棕色块1
+            var fillColor = color.fromHEX("#491b18");
+            graphics.rect(5, 190, width - 10, 60);
+            graphics.fillColor = fillColor;
+            graphics.fill();
+
+            //底部棕色块2
+            var fillColor = color.fromHEX("#441a18");
+            graphics.rect(5, 120, width - 10, 70);
+            graphics.fillColor = fillColor;
+            graphics.fill();
+
+            //底部棕色块3
+            var fillColor = color.fromHEX("#351516");
+            graphics.rect(5, 0, width - 10, 121);
+            graphics.fillColor = fillColor;
+            graphics.fill();
 
             //create red land.
             var redLand = new cc.Node("Red Land");
@@ -48,7 +86,8 @@ var spriteCreator = function () {
             var sprite = stick.addComponent(cc.Sprite);
             sprite.sizeMode = cc.Sprite.SizeMode.CUSTOM;
             sprite.spriteFrame = spriteFrameCache;
-            stick.color = cc.Color.BLACK;
+            var stickColor = cc.Color.BLACK;
+            stick.color = stickColor.fromHEX("#ef9d24");
             stick.width = width;
             stick.height = 0;
             return stick;
